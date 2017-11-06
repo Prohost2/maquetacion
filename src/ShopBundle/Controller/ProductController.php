28bb4c149b3dca3d2ea4bc156c3b787e1162ae36
SamuelCampo <31445815+SamuelCampo@users.noreply.github.com>
2017-11-06 07:01:52 -0430

    Modificaciones de responsive

--- src/ShopBundle/Controller/ProductController.php
+++ src/ShopBundle/Controller/ProductController.php
@@ -7,24 +7,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ProductController extends Controller
 {
     /*
-    * 
+    * Route: shop/product
     */
     public function indexAction()
     {
         return $this->render('ShopBundle:Product:index.html.twig');
     }
+    /*
+    * Route: shop/check
+    */    
     public function checkAction()
     {
         return $this->render('ShopBundle:Product:checkout_one.html.twig');
     }
+    /*
+    * Route: shop/info
+    */
     public function infoAction()
     {
         return $this->render('ShopBundle:Product:checkout_two.html.twig');
     }
+    /*
+    * Route: shop/payments
+    */
     public function paymentsAction()
     {
         return $this->render('ShopBundle:Product:checkout_three.html.twig');
     }
+    /*
+    * Route: shop/confirm_payments
+    */
     public function confirm_paymentsAction()
     {
         return $this->render('ShopBundle:Product:checkout_four.html.twig');
