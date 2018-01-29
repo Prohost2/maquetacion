8f21f1c24e2d7eb3d78f566fcf44af79f555cc1f
SamuelCampo <31445815+SamuelCampo@users.noreply.github.com>
2018-01-29 17:26:04 -0430

    Modifiaciones del menu con imagenes

--- web/js/function.js
+++ web/js/function.js
@@ -16,6 +16,18 @@ $(document).ready(function() {
     $("#photo_principal").attr('src', url);
 
   });
+
+  $('.dropdown-item').hover(function() {
+    $('.muestra').attr({
+      src: 'http://34.230.134.197/amalia/web/images/amazon1/8457_az1.jpg',
+      class: 'img-responsive',
+      width: '150rem'
+    });
+    alert($(this).attr('id'));
+  });
 });
+
+
+
 
   $(document).ready(function() { $('body').bootstrapMaterialDesign(); });
